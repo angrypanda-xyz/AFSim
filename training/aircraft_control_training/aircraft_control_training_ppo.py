dff10312bfc429d81748a06276224d8c799324de
SCU101 <wu1126451664@gmail.com>
2026-04-09 15:43:34 +0800

    更新了飞机姿态控制多进程训练代码PPO示例

--- training/aircraft_control_training/aircraft_control_training_ppo.py
+++ training/aircraft_control_training/aircraft_control_training_ppo.py
@@ -1,22 +1,24 @@
 from stable_baselines3 import PPO
 from stable_baselines3.common.monitor import Monitor
-from stable_baselines3.common.vec_env import DummyVecEnv
 from stable_baselines3.common.callbacks import CheckpointCallback
+from stable_baselines3.common.vec_env import DummyVecEnv, VecNormalize, SubprocVecEnv
 from communication.tcp_client import SimulationClient
 from environments.aircraft_control.aircraft_control_env import AircraftControlEnv
+from functools import partial
 
 
-def make_env(log_save=False):
-    simulation = SimulationClient(host='127.0.0.1', port=8888, steps=6, env_name="control", log_save=log_save)
-    # env = AircraftControlEnv(simulation_client=simulation, max_steps=800, render_mode="human")
-    environment = AircraftControlEnv(simulation_client=simulation, max_steps=400, random_init=True)
-    return environment
+def make_env(steps, max_steps, log_save=False):
+    simulation = SimulationClient(host='127.0.0.1', port=8888, steps=steps, environment="control", log_save=log_save)
+    environment = AircraftControlEnv(simulation_client=simulation, max_steps=max_steps, random_init=True)
+    return Monitor(environment)
 
 
 if __name__ == "__main__":
     train = True
     if train:
-        env = DummyVecEnv([make_env])
+        env_fns = [partial(make_env, steps=20, max_steps=1000, log_save=False) for _ in range(5)]
+        env = SubprocVecEnv(env_fns)
+        # env = DummyVecEnv([make_env])
         # 创建模型
         model = PPO(
             "MlpPolicy",
@@ -38,7 +40,7 @@ if __name__ == "__main__":
             model.learn(total_timesteps=1000000)
             model.save("ppo_point_tracking")
     else:
-        env = make_env(log_save=True)
+        env = make_env(steps=20, max_steps=1000, log_save=True)
         model = PPO.load("ppo_point_tracking", env=env, device='cpu')
         # 测试
         obs, info = env.reset()
